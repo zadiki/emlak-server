c7cc1d394064cdc719aebf03ed94855bd06fcde8
zadiki ochola <zadikiochola@gmail.com>
2018-09-12 21:37:20 +0300

    db connection for gcp

--- app.js
+++ app.js
@@ -43,13 +43,19 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, '/app/views'));
 app.set('view engine', 'ejs');
 
-mongoose.connect(Constants.MONGODB_URL,function (err,db) {
-    if(err){
-        console.log("failed to connect to mongo db");
-    }else{
-        console.log("db connection successfull")
-    }
-});
+const options = {
+    useNewUrlParser: true,
+    autoIndex: false, // Don't build indexes
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0,
+    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+};
+mongoose.connect(Constants.MONGODB_URL,options);
 
 app.use(session({
     secret: 'iProcureSecretCode',
